b9f63c62098224afbc114a9e8d4e9040566a0194
Shea Phillips <shea.phillips@gmail.com>
2021-06-07 11:56:51 -0700

    - removing creationg network association and authorization elements, as teams have the knowledge to create these and should have control over when they are on/off.

--- module/main.tf
+++ module/main.tf
@@ -52,29 +52,3 @@ resource "aws_ec2_client_vpn_endpoint" "vpn_endpoint" {
 		enabled = false
 	}
 }
-
-module "network-info" {
-	source = "git::git@github.com:BCDevOps/terraform-octk-aws-sea-network-info?ref=v0.0.2"
-
-	environment = var.environment
-}
-
-
-// todo network association and auth rule should be refactored so they can be created based on project configuration and/or not creat at all but overlay, and delgated to tenant teams
-
-resource "aws_ec2_client_vpn_network_association" "associations" {
-	for_each = toset([for subnet in module.network-info.aws_subnet.data : subnet.id])
-	client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.vpn_endpoint.id
-	subnet_id = each.value
-	security_groups = [
-		module.network-info.aws_security_groups.app.id]
-}
-
-resource "aws_ec2_client_vpn_authorization_rule" "auth_data_subnets" {
-	for_each = toset([for subnet in module.network-info.aws_subnet.data : subnet.cidr_block])
-	client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.vpn_endpoint.id
-	target_network_cidr = each.value
-	//	todo convention for creating roles in KC to allow for different access level
-	access_group_id = var.vpn_users_group_name
-}
-
